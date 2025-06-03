fadaced0c9f3420a1127c23f08635d57f4823351
zoiska <zoisk64@gmail.com>
2025-06-03 17:30:55 +0200

    dynamic audio volume, changed event to input, removed redundant functions

--- js/audio.js
+++ js/audio.js
@@ -1,50 +1,44 @@
 let audio = document.getElementById("bgaudio");
 let volume = document.getElementById("volume-slider");
 let buysound = document.getElementById("buysound");
-let fxvolume = document.getElementById("volume-slider-effects")
-let buttonfx = document.getElementById("buttonpresssound")
-let mainbuttonfx = document.getElementById("mainclicksound")
+let fxvolume = document.getElementById("volume-slider-effects");
+let buttonfx = document.getElementById("buttonpresssound");
+let mainbuttonfx = document.getElementById("mainclicksound");
 
 audio.pause();
 
 // attention: volume, as in sound, may or may not be the same as the variable 'volume'
-volume.addEventListener("change", function (e) {
-  audio.volume = e.currentTarget.value / 100;
+volume.addEventListener("input", (e) => {
+  audio.volume = e.target.value / 100;
 });
 
-fxvolume.addEventListener("change", function (f) {
-  buysound.volume = f.currentTarget.value / 100;
+fxvolume.addEventListener("input", (e) => {
+  buysound.volume = e.target.value / 100;
+  mainbuttonfx.volume = e.target.value / 100;
+  buttonfx.volume = e.target.value / 100;
 });
 
-fxvolume.addEventListener("change", function (g) {
-  buttonfx.volume = g.currentTarget.value / 100;
-});
-
-fxvolume.addEventListener("change", function (h) {
-  mainbuttonfx.volume = h.currentTarget.value / 100;
-});
-
-export function buysoundclick(){
-  buysound.currentTime=0;
+export function buysoundclick() {
+  buysound.currentTime = 0;
   buysound.play();
 }
 
-export function mainbuttonclicksound(){
-  mainbuttonfx.currentTime=0;
+export function mainbuttonclicksound() {
+  mainbuttonfx.currentTime = 0;
   mainbuttonfx.play();
 }
 
-export function buttonclicksound(){
-  buttonfx.currentTime=0;
+export function buttonclicksound() {
+  buttonfx.currentTime = 0;
   buttonfx.play();
 }
 
 export function standardvolume() {
-    buttonfx.volume = 0.2;
-    mainbuttonfx.volume = 0.2;
-    buysound.volume = 0.2;
-    audio.volume = 0.2;
-    audio.play();
+  buttonfx.volume = 0.2;
+  mainbuttonfx.volume = 0.2;
+  buysound.volume = 0.2;
+  audio.volume = 0.2;
+  audio.play();
 }
 
 export function unmuteMute() {
