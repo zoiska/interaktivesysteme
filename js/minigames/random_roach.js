27e701f1adeb7defea2896d1e2f2b9b9ed59d976
zoiska <zoisk64@gmail.com>
2025-06-03 23:22:33 +0200

    add random event/minigame bug squasher

---
+++ js/minigames/random_roach.js
@@ -0,0 +1,84 @@
+import { cockroach_svg } from "../../emojisvg/cockroach_svg.js";
+
+let prevX = 0;
+let prevY = 0;
+
+function makeRoach() {
+  const area = document.querySelector(".roach_area");
+
+  const roach = document.createElement("div");
+  roach.className = "roach";
+  roach.innerHTML = cockroach_svg;
+
+  area.appendChild(roach);
+
+  requestAnimationFrame(() => {
+    roach.classList.add("show");
+  });
+
+  roach.addEventListener("click", () => {
+    roach.classList.remove("show");
+    roach.classList.add("fade-out");
+
+    roach.addEventListener("transitioned", () => {
+      roach.remove();
+    });
+
+    //logic
+
+    // play sound
+  });
+
+  const offsetX = Math.floor((Math.random() * window.innerWidth) / 2);
+  const offsetY = Math.floor((Math.random() * window.innerHeight) / 2);
+
+  const amplitudeX1 = Math.floor(Math.random() * 100) + 50;
+  const amplitudeX2 = Math.floor(Math.random() * 100) + 50;
+
+  const amplitudeY1 = Math.floor(Math.random() * 100) + 50;
+  const amplitudeY2 = Math.floor(Math.random() * 100) + 50;
+
+  const frequencyX1 = Math.random() * 2 + 0.1;
+  const frequencyX2 = Math.random() * 2 + 0.1;
+
+  const frequencyY1 = Math.random() * 2 + 0.1;
+  const frequencyY2 = Math.random() * 2 + 0.1;
+
+  const phaseX1 = Math.random() * 2 * Math.PI;
+  const phaseX2 = Math.random() * 2 * Math.PI;
+  const phaseY1 = Math.random() * 2 * Math.PI;
+  const phaseY2 = Math.random() * 2 * Math.PI;
+
+  function animateRoach() {
+    const time = Date.now() / 1000; // seconds
+
+    const x =
+      offsetX +
+      amplitudeX1 * Math.sin(frequencyX1 * time + phaseX1) +
+      amplitudeX2 * Math.sin(frequencyX2 * time + phaseX2);
+
+    const y =
+      offsetY +
+      amplitudeY1 * Math.sin(frequencyY1 * time + phaseY1) +
+      amplitudeY2 * Math.sin(frequencyY2 * time + phaseY2);
+
+    const angle = Math.atan2(x - prevX, y - prevY) * (180 / Math.PI);
+
+    roach.style.transform = `translate(${x}px, ${y}px)` + `rotate(${angle}deg)`;
+
+    prevX = x;
+    prevY = y;
+
+    requestAnimationFrame(animateRoach);
+  }
+
+  animateRoach();
+}
+
+(function loop() {
+  var rand = Math.round(Math.random() * (10 * 10000) + 10 * 10000);
+  setTimeout(() => {
+    makeRoach();
+    loop();
+  }, rand);
+})();
