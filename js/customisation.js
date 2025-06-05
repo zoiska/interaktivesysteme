a8a2d13cb12df6016f26c5803ae333220cb75399
zoiska <zoisk64@gmail.com>
2025-06-05 15:01:38 +0200

    finalize TODO, add ruby

--- js/customisation.js
+++ js/customisation.js
@@ -2,5 +2,5 @@ import { state } from "./config.js";
 
 export function updateCustomisation() {
   const extraCurrency = document.querySelector("#extraCurrency");
-  extraCurrency.innerHTML = `<img src="./emojisvg/coin_1FA99.svg">` + state.extraCurrency;
+  extraCurrency.innerHTML = `<img src="./emojisvg/ruby_E04F.svg">` + state.extraCurrency;
 }
