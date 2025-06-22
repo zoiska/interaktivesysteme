8a048ff36ae752ff329a2cdd4764861b2423fdaa
zoiska <zoisk64@gmail.com>
2025-06-22 23:42:59 +0200

    delete function

--- js/svgInjector.js
+++ js/svgInjector.js
@@ -98,41 +98,6 @@ export function injectItems() {
   });
 }
 
-export function transformClicker() {
-  const clickable = document.querySelector("#clickableArea");
-  const clickable1 = document.querySelector("#clickableAreaHat");
-
-  clickable.addEventListener("pointerdown", () => {
-    clickable.style.transform = "scale(1.1)";
-    clickable1.style.transform = "scale(1.1)";
-  });
-
-  clickable.addEventListener("pointerup", () => {
-    clickable.style.transform = "scale(1)";
-    clickable1.style.transform = "scale(1)";
-  });
-
-  clickable.addEventListener("pointerleave", () => {
-    clickable.style.transform = "scale(1)";
-    clickable1.style.transform = "scale(1)";
-  });
-
-  clickable1.addEventListener("pointerdown", () => {
-    clickable.style.transform = "scale(1.1)";
-    clickable1.style.transform = "scale(1.1)";
-  });
-
-  clickable1.addEventListener("pointerup", () => {
-    clickable.style.transform = "scale(1)";
-    clickable1.style.transform = "scale(1)";
-  });
-
-  clickable1.addEventListener("pointerleave", () => {
-    clickable.style.transform = "scale(1)";
-    clickable1.style.transform = "scale(1)";
-  });
-}
-
 export function injectSplat(area, x, y) {
   const splat = document.createElement("div");
   splat.className = "splat";
